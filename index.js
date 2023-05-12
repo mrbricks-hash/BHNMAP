f3035370b55c75792223f831b4a9b81cef7b00ca
mrbricks-hash <85583538+mrbricks-hash@users.noreply.github.com>
2023-05-12 18:52:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,80 @@
-const Telegraf = require('telegraf');
-const { JSSoup } = require('jssoup');
-const { Nmap } = require('node-nmap');
+const TelegramBot = require('node-telegram-bot-api');
+const JSSoup = require('jssoup').default;
+const nmap = require('node-nmap');
 
-const bot = new Telegraf('5871264620:AAHzJB0P1vEDue0NCEY8DwNbC335PQokoAE');
+// Telegram Bot Token
+const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+const bot = new TelegramBot(token, { polling: true });
 
-// Start command handler
-bot.command('start', (ctx) => {
-  ctx.replyWithMarkdownV2('Push start to work magic. \n\n💻🎩');
+// Handler for /start command and initial message
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+
+  const startMessage = 'Push start to work magic.';
+
+  // Inline markup keyboard
+  const options = {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: 'Start', callback_data: 'start' }]
+      ]
+    }
+  };
+
+  bot.sendMessage(chatId, startMessage, options);
 });
 
-// Text message handler
-bot.on('text', (ctx) => {
-  if (ctx.message.text.startsWith('http://') || ctx.message.text.startsWith('https://')) {
-    const targetWebsite = ctx.message.text;
-    
-    ctx.reply('Blackhat\'s working his magic. ⚡️');
+// Callback query handler
+bot.on('callback_query', (callbackQuery) => {
+  const chatId = callbackQuery.message.chat.id;
+  const messageId = callbackQuery.message.message_id;
 
-    // Perform Nmap scan
-    const nmap = new Nmap();
-    nmap.scan(targetWebsite, ['-p', '80,443'], (err, report) => {
+  if (callbackQuery.data === 'start') {
+    bot.sendMessage(chatId, "Enter the target website you'd like to map:");
+  }
+
+  // User input handler
+  bot.onText(/.+/, (msg) => {
+    const targetWebsite = msg.text;
+    const options = {
+      target: targetWebsite,
+      flags: '-p 80,443' // Scan HTTP and HTTPS ports
+    };
+
+    bot.sendMessage(chatId, 'Blackhat\'s working his magic.');
+
+    nmap.scan(options, (err, report) => {
       if (err) {
-        ctx.reply('Oops! Something went wrong while scanning the target website. Please try again later. ❌');
+        bot.sendMessage(chatId, 'An error occurred while scanning the website.');
         console.error(err);
         return;
       }
 
-      const emailSet = new Set();
+      const extractedEmails = extractEmails(report);
 
-      // Extract emails using JSSoup
-      const soup = new JSSoup(report[0].data);
-      const emailElements = soup.findAll('a[href^="mailto:"]');
-      
-      emailElements.forEach((element) => {
-        const email = element.attrs.href.replace('mailto:', '');
-        emailSet.add(email);
-      });
-
-      if (emailSet.size === 0) {
-        ctx.reply('No emails found on the target website. 😕');
+      if (extractedEmails.length === 0) {
+        bot.sendMessage(chatId, 'No emails found.');
       } else {
-        const emailList = Array.from(emailSet);
-        ctx.reply(`Emails found on the target website:\n\n${emailList.join('\n')}`);
+        const resultMessage = `Found emails:\n${extractedEmails.join('\n')}`;
+        bot.sendMessage(chatId, resultMessage);
       }
 
-      // Ask if user wants to conduct another scrape
-      ctx.replyWithMarkdownV2('Would you like to conduct another scrape? \n\n✨ Yes, please /start another scrape.\n\n🚫 No, thanks.');
+      // Ask if the user wants to conduct another scrape
+      const anotherScrapeMessage = 'Would you like to conduct another scrape?';
+      const options = {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: 'Yes', callback_data: 'start' }],
+            [{ text: 'No', callback_data: 'end' }]
+          ]
+        }
+      };
+
+      bot.sendMessage(chatId, anotherScrapeMessage, options);
     });
-  } else {
-    ctx.reply('Please enter a valid URL starting with http:// or https://. 🌐');
-  }
-});
+  });
 
-// Handle 'Yes, please' response
-bot.command('start', (ctx) => {
-  ctx.replyWithMarkdownV2('Sure! Enter the target website URL you\'d like to map.');
-});
-
-// Handle 'No, thanks' response
-bot.command('stop', (ctx) => {
-  ctx.reply('Thank you for using Blackhats Nmapper! If you have any other questions, feel free to ask. 👋');
-});
-
-// Start the bot
-bot.launch();
+  // Extract emails from the HTML using JSSoup
+  function extractEmails(report) {
+    const soup = new JSSoup(report[0].data);
+    const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|
