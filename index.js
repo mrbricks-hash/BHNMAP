2ef6cbef59d81dff583c7082989f52df48c053af
mrbricks-hash <85583538+mrbricks-hash@users.noreply.github.com>
2023-05-12 18:54:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,80 +1,67 @@
 const TelegramBot = require('node-telegram-bot-api');
-const JSSoup = require('jssoup').default;
-const nmap = require('node-nmap');
+const Nmap = require('node-nmap');
+const request = require('request');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
-// Telegram Bot Token
-const token = '5871264620:AAHzJB0P1vEDue0NCEY8DwNbC335PQokoAE';
-const bot = new TelegramBot(token, { polling: true });
+// Replace 'YOUR_TELEGRAM_BOT_TOKEN' with your actual Telegram bot token
+const bot = new TelegramBot('5871264620:AAHzJB0P1vEDue0NCEY8DwNbC335PQokoAE', { polling: true });
 
-// Handler for /start command and initial message
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-
-  const startMessage = 'Push start to work magic.';
-
-  // Inline markup keyboard
+  const message = 'Push start to work magic.';
   const options = {
     reply_markup: {
-      inline_keyboard: [
-        [{ text: 'Start', callback_data: 'start' }]
-      ]
-    }
+      inline_keyboard: [[{ text: 'Start', callback_data: 'start' }]],
+    },
   };
 
-  bot.sendMessage(chatId, startMessage, options);
+  bot.sendMessage(chatId, message, options);
 });
 
-// Callback query handler
-bot.on('callback_query', (callbackQuery) => {
-  const chatId = callbackQuery.message.chat.id;
-  const messageId = callbackQuery.message.message_id;
+bot.on('callback_query', (query) => {
+  const chatId = query.message.chat.id;
+  const messageId = query.message.message_id;
 
-  if (callbackQuery.data === 'start') {
+  if (query.data === 'start') {
     bot.sendMessage(chatId, "Enter the target website you'd like to map:");
-  }
+    bot.onReplyToMessage(chatId, messageId, (msg) => {
+      const targetWebsite = msg.text;
 
-  // User input handler
-  bot.onText(/.+/, (msg) => {
-    const targetWebsite = msg.text;
-    const options = {
-      target: targetWebsite,
-      flags: '-p 80,443' // Scan HTTP and HTTPS ports
-    };
+      bot.sendMessage(chatId, 'Blackhat is working his magic... ⚡️');
 
-    bot.sendMessage(chatId, 'Blackhat\'s working his magic.');
+      const nmap = new Nmap();
 
-    nmap.scan(options, (err, report) => {
-      if (err) {
-        bot.sendMessage(chatId, 'An error occurred while scanning the website.');
-        console.error(err);
-        return;
-      }
-
-      const extractedEmails = extractEmails(report);
-
-      if (extractedEmails.length === 0) {
-        bot.sendMessage(chatId, 'No emails found.');
-      } else {
-        const resultMessage = `Found emails:\n${extractedEmails.join('\n')}`;
-        bot.sendMessage(chatId, resultMessage);
-      }
-
-      // Ask if the user wants to conduct another scrape
-      const anotherScrapeMessage = 'Would you like to conduct another scrape?';
-      const options = {
-        reply_markup: {
-          inline_keyboard: [
-            [{ text: 'Yes', callback_data: 'start' }],
-            [{ text: 'No', callback_data: 'end' }]
-          ]
+      nmap.scan(targetWebsite, '-p 80,443', (err, report) => {
+        if (err) {
+          bot.sendMessage(chatId, 'An error occurred while scanning the target.');
+          console.error(err);
+          return;
         }
-      };
 
-      bot.sendMessage(chatId, anotherScrapeMessage, options);
+        const scanResult = report[0];
+        const openPorts = scanResult.openPorts;
+
+        const url = `https://${targetWebsite}`;
+        request(url, (error, response, body) => {
+          if (error) {
+            bot.sendMessage(chatId, 'An error occurred while scraping the target website.');
+            console.error(error);
+            return;
+          }
+
+          const dom = new JSDOM(body);
+          const emails = dom.window.document.querySelectorAll('a[href^="mailto:"]');
+          const emailList = Array.from(emails).map((email) => email.href.substring(7));
+
+          if (emailList.length > 0) {
+            const resultMessage = `Found ${emailList.length} emails:\n\n${emailList.join('\n')}`;
+            bot.sendMessage(chatId, resultMessage);
+          } else {
+            bot.sendMessage(chatId, 'No emails found on the target website.');
+          }
+        });
+      });
     });
-  });
-
-  // Extract emails from the HTML using JSSoup
-  function extractEmails(report) {
-    const soup = new JSSoup(report[0].data);
-    const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|
+  }
+});
